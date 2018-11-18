b687e8cb165f0748f46aa6446eec17b32b9c5237
10akki <45142205+10akki@users.noreply.github.com>
2018-11-18 21:06:24 +0530

    create pull request

--- src/main/java/com/fis/GeneratePdf.java
+++ src/main/java/com/fis/GeneratePdf.java
@@ -140,7 +140,6 @@ public class GeneratePdf {
 			cell.setHorizontalAlignment(Element.ALIGN_CENTER);
 			cell.setPhrase(phrase);
 			cell.setBorderWidth(1);
-			
 			table.addCell(cell);
 		}
 		return table;
